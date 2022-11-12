1123bcef87bdbb329a7b9e6fb544fb6f9128a439
2C24HaydenS <115450543+2C24HaydenS@users.noreply.github.com>
2022-11-12 22:07:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
     led.plotBarGraph(
-    0,
-    0
+    input.soundLevel(),
+    255
     )
 })
